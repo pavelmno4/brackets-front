deaebcb9c2e4c7e0990348be7df7d1fd6bffd84b
Pavel Kozlov <pavel.mno4@gmail.com>
2024-09-13 19:31:48 +0300

    fetch participants data from server

--- src/routes/competition/[id]/participants/[gender]/[[year]]/[[weight]]/+page.server.ts
+++ src/routes/competition/[id]/participants/[gender]/[[year]]/[[weight]]/+page.server.ts
@@ -1,53 +1,17 @@
+import { GET } from "$lib/api/ApiUtils";
+import { mapToCompetition } from "$lib/mapper/CompetitionMapper";
+
+
+
 export async function load({ params }) {
-    return params.gender === 'FEMALE' ?
-        {
-            participants: [
-                {
-                    'id': '1',
-                    'fullName': 'Максимова Евгения',
-                    'birthYear': 2011,
-                    'weight': 42.4,
-                    'team': 'Лесные защитники'
-                },
-                {
-                    'id': '2',
-                    'fullName': 'Ульянцева Арина',
-                    'birthYear': 2015,
-                    'weight': 34.4,
-                    'team': 'Аaaaaaaaaaaaaaaaaaaaaaaaaaaaaaдреналин Team'
-                },
-                {
-                    'id': '3',
-                    'fullName': 'Спиридонова Наталья',
-                    'birthYear': 2010,
-                    'weight': 39.5,
-                    'team': 'Ромашки'
-                }
-            ]
-        } :
-        {
-            participants: [
-                {
-                    'id': '1',
-                    'fullName': 'Савушкин Максим',
-                    'birthYear': 2011,
-                    'weight': 54.4,
-                    'team': 'Аaaaaaaaaaaaaaaaaaaaaaaaaaaaaaдреналин Team'
-                },
-                {
-                    'id': '2',
-                    'fullName': 'Прохоров Константин',
-                    'birthYear': 2012,
-                    'weight': 53.8,
-                    'team': 'Рыжий хвост'
-                },
-                {
-                    'id': '3',
-                    'fullName': 'Прокофьев Александр',
-                    'birthYear': 2011,
-                    'weight': 54.1,
-                    'team': 'Молот'
-                }
-            ]
-        };
+    const url = new URL(`http://localhost:8080/competitions/${params.id}/participants`);
+    url.searchParams.set('gender', params.gender);
+    if (params.year !== undefined) url.searchParams.set('ageCategory', params.year);
+    if (params.weight !== undefined) url.searchParams.set('weightCategory', params.weight);
+
+    const participants = await GET(url.href)
+
+    return {
+        participants: participants
+    };
 }
