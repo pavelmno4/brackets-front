eb05b3b60930a3d0dd4463ff6e1b663e0684bf26
Pavel Kozlov <pavel.mno4@gmail.com>
2024-10-24 20:32:44 +0300

    use competition.imagePath

--- src/lib/CompetitionCard.svelte
+++ src/lib/CompetitionCard.svelte
@@ -1,63 +1,71 @@
 <script lang="ts">
-    import { locale } from '$lib/const';
+	import { locale } from '$lib/const';
 
-    import image from '$lib/test/competition-image.jpg';
-
-    export let id: string;
-    export let title: string;
-    export let startDate: Date;
-    export let endDate: Date;
-    export let address: string;
-    export let imagePath: string;
+	export let id: string;
+	export let title: string;
+	export let startDate: Date;
+	export let endDate: Date;
+	export let address: string;
+	export let imagePath: string;
 </script>
 
 <a href="/competition/{id}" class="competiton-card">
-    <article class="card">
-        <img src={image} alt="Fight of two wrestlers" />
-        <div class="description">
-            <h2>{title}</h2>
-            <p>
-                {#if startDate.getUTCDate() === endDate.getUTCDate()}
-                    {startDate.toLocaleDateString(locale, { day: "numeric", month: "short", year: "numeric" })}
-                {:else if startDate.getMonth() === endDate.getMonth()}
-                    {startDate.getDate()}-{endDate.getDate()} {endDate.toLocaleString(locale, { month: "short" })} {endDate.getFullYear()} г.
-                {:else}
-                    {startDate.getDate()} {startDate.toLocaleString(locale, { month: "short" })} - {endDate.getDate()} {endDate.toLocaleString(locale, { month: "short" })} {endDate.getFullYear()} г.
-                {/if}
-            </p>
-            <p>{address}</p>
-        </div>
-    </article>
+	<article class="card">
+		<img src={imagePath} alt="Fight of two wrestlers" />
+		<div class="description">
+			<h2>{title}</h2>
+			<p>
+				{#if startDate.getUTCDate() === endDate.getUTCDate()}
+					{startDate.toLocaleDateString(locale, {
+						day: 'numeric',
+						month: 'short',
+						year: 'numeric'
+					})}
+				{:else if startDate.getMonth() === endDate.getMonth()}
+					{startDate.getDate()}-{endDate.getDate()}
+					{endDate.toLocaleString(locale, { month: 'short' })}
+					{endDate.getFullYear()} г.
+				{:else}
+					{startDate.getDate()}
+					{startDate.toLocaleString(locale, { month: 'short' })} -
+					{endDate.getDate()}
+					{endDate.toLocaleString(locale, { month: 'short' })}
+					{endDate.getFullYear()} г.
+				{/if}
+			</p>
+			<p>{address}</p>
+		</div>
+	</article>
 </a>
 
 <style>
-    a {
-        display: block;
-        text-decoration: none;
-    }
+	a {
+		display: block;
+		text-decoration: none;
+	}
 
-    .card {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: flex-start;
-    }
+	.card {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: flex-start;
+	}
 
-    img {
-        height: 30vh;
-        border: 1px solid black;
-        border-radius: 6px;
-    }
+	img {
+		height: 30vh;
+		border: 1px solid black;
+		border-radius: 6px;
+	}
 
-    .description {
-        margin: 3vh;
-    }
+	.description {
+		margin: 3vh;
+	}
 
-    @media all and (max-width: 1024px) {
-        .card {
-            padding-left: 0;
-            padding-right: 0;
-            flex-direction: column;
-            align-items: center;
-            }
-    }
+	@media all and (max-width: 1024px) {
+		.card {
+			padding-left: 0;
+			padding-right: 0;
+			flex-direction: column;
+			align-items: center;
+		}
+	}
 </style>
--- src/routes/competition/[id]/+page.svelte
+++ src/routes/competition/[id]/+page.svelte
@@ -1,109 +1,117 @@
 <script lang="ts">
-    import { locale } from '$lib/const';
-
-    import { page } from '$app/stores';
-    import type { Competition } from '$lib/types/competition/Competition';
-    import CategoryTable from './CategoryTable.svelte';
-    import image from '$lib/test/competition-image.jpg';
+	import { locale } from '$lib/const';
+	import { page } from '$app/stores';
+	import type { Competition } from '$lib/types/competition/Competition';
+	import CategoryTable from './CategoryTable.svelte';
 	import type { PageData } from './$types';
 
-    export let data: PageData;
+	export let data: PageData;
 
-    $: competition = data.competition satisfies Competition;
-    $: registrationUrl = $page.url + '/registration';
-    $: femaleParticipantsUrl = $page.url + '/participants/FEMALE';
+	$: competition = data.competition satisfies Competition;
+	$: registrationUrl = $page.url + '/registration';
+	$: femaleParticipantsUrl = $page.url + '/participants/FEMALE';
 </script>
 
 <section class="competition">
-    <article class="card">
-        <h1 class="title">{competition.title}</h1>
-        <div class="image-and-description">
-            <img src={image} alt="Fight of two wrestlers" />
-            <div class="description">
-                <p>
-                    {#if competition.startDate.getUTCDate() === competition.endDate.getUTCDate()}
-                        {competition.startDate.toLocaleDateString(locale, { day: "numeric", month: "short", year: "numeric" })}
-                    {:else if competition.startDate.getMonth() === competition.endDate.getMonth()}
-                        {competition.startDate.getDate()}-{competition.endDate.getDate()} {competition.endDate.toLocaleString(locale, { month: "short" })} {competition.endDate.getFullYear()} г.
-                    {:else}
-                        {competition.startDate.getDate()} {competition.startDate.toLocaleString(locale, { month: "short" })} - {competition.endDate.getDate()} {competition.endDate.toLocaleString(locale, { month: "short" })} {competition.endDate.getFullYear()} г.
-                    {/if}
-                </p>
-                <p>{competition.address}</p>
-                <a href={registrationUrl} class="registration-link">Регистрация участника</a>
-            </div>
-        </div>
-        <h4 class="participants-list-title">Списки участников</h4>
-        <h6 class="male-participants">Юноши</h6>
-        <CategoryTable categories={competition.categories} />
-        <h6 class="female-participants">Девушки</h6>
-        <a href="{femaleParticipantsUrl}">Смотреть список участниц</a>
-    </article>
+	<article class="card">
+		<h1 class="title">{competition.title}</h1>
+		<div class="image-and-description">
+			<img src={competition.imagePath} alt="Fight of two wrestlers" />
+			<div class="description">
+				<p>
+					{#if competition.startDate.getUTCDate() === competition.endDate.getUTCDate()}
+						{competition.startDate.toLocaleDateString(locale, {
+							day: 'numeric',
+							month: 'short',
+							year: 'numeric'
+						})}
+					{:else if competition.startDate.getMonth() === competition.endDate.getMonth()}
+						{competition.startDate.getDate()}-{competition.endDate.getDate()}
+						{competition.endDate.toLocaleString(locale, { month: 'short' })}
+						{competition.endDate.getFullYear()} г.
+					{:else}
+						{competition.startDate.getDate()}
+						{competition.startDate.toLocaleString(locale, { month: 'short' })} -
+						{competition.endDate.getDate()}
+						{competition.endDate.toLocaleString(locale, { month: 'short' })}
+						{competition.endDate.getFullYear()} г.
+					{/if}
+				</p>
+				<p>{competition.address}</p>
+				<a href={registrationUrl} class="registration-link">Регистрация участника</a>
+			</div>
+		</div>
+		<h4 class="participants-list-title">Списки участников</h4>
+		<h6 class="male-participants">Юноши</h6>
+		<CategoryTable categories={competition.categories} />
+		<h6 class="female-participants">Девушки</h6>
+		<a href={femaleParticipantsUrl}>Смотреть список участниц</a>
+	</article>
 </section>
 
 <style>
-    .card {
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-        align-items: flex-start;
-    }
+	.card {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		align-items: flex-start;
+	}
 
-    .title {
-        align-self: center;
-        text-align: center;
-    }
+	.title {
+		align-self: center;
+		text-align: center;
+	}
 
-    .image-and-description {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
+	.image-and-description {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
 
-    img {
-        height: 30vh;
-        border: 1px solid black;
-        border-radius: 6px;
-    }
+	img {
+		height: 30vh;
+		border: 1px solid black;
+		border-radius: 6px;
+	}
 
-    .description {
-        margin: 3vh;
-    }
+	.description {
+		margin: 3vh;
+	}
 
-    .registration-link {
-        color: black;
-        background-color: var(--pico-color-azure-100);
-        border: 1px solid gray;
-        border-radius: 6px;
-        width: 100%;
-        padding: 14px 25px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-    }
+	.registration-link {
+		color: black;
+		background-color: var(--pico-color-azure-100);
+		border: 1px solid gray;
+		border-radius: 6px;
+		width: 100%;
+		padding: 14px 25px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+	}
 
-    .registration-link:hover {
-        background-color: var(--pico-color-azure-150);
-    }
+	.registration-link:hover {
+		background-color: var(--pico-color-azure-150);
+	}
 
-    .participants-list-title {
-        margin-top: 2vh;
-        font-weight: 500;
-        text-decoration: underline;
-    }
+	.participants-list-title {
+		margin-top: 2vh;
+		font-weight: 500;
+		text-decoration: underline;
+	}
 
-    .male-participants {
-        font-weight: 500;
-    }
+	.male-participants {
+		font-weight: 500;
+	}
 
-    .female-participants {
-        font-weight: 500;
-    }
+	.female-participants {
+		font-weight: 500;
+	}
 
-    @media all and (max-width: 1024px) {
-        .card {
-            align-items: center;
-        }
-    }
+	@media all and (max-width: 1024px) {
+		.card {
+			align-items: center;
+		}
+	}
 </style>
