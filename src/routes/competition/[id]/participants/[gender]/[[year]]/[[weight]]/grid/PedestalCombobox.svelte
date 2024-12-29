fb4eeceaf62ef1e4fc263277b820d8e001ed19f3
Pavel Kozlov <pavel.mno4@gmail.com>
2024-12-29 19:32:19 +0300

    fix pedestal selection

--- src/routes/competition/[id]/participants/[gender]/[[year]]/[[weight]]/grid/PedestalCombobox.svelte
+++ src/routes/competition/[id]/participants/[gender]/[[year]]/[[weight]]/grid/PedestalCombobox.svelte
@@ -15,8 +15,10 @@
 		(participant) => participant.id === filledParticipantId
 	);
 
-	let selected: Selected<string> | undefined;
-	let inputValue: string = filledParticipant ? filledParticipant.fullName : '';
+	let selected: Selected<string> | undefined = filledParticipant
+		? { value: filledParticipant.id, label: filledParticipant.fullName }
+		: undefined;
+	let inputValue: string;
 	let touchedInput = false;
 	$: filteredParticipants =
 		inputValue && touchedInput
@@ -29,6 +31,10 @@
 <Combobox.Root items={filteredParticipants} bind:inputValue bind:touchedInput bind:selected>
 	<Combobox.Input class="combobox-input" {placeholder} />
 	<Combobox.Content class="combobox-content" sideOffset={8} side="top">
+		<Combobox.Item class="combobox-item" value="undefined" label="Отсутствует">
+			Отсутствует
+			<Combobox.ItemIndicator class="indicator" asChild={false} />
+		</Combobox.Item>
 		{#each filteredParticipants as participant}
 			<Combobox.Item class="combobox-item" value={participant.value} label={participant.label}>
 				{participant.label}
